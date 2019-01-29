b9a2aef79d6213974cb5e38830deb9890e666278
Sean Collins <sc679115@ohio.edu>
2019-01-29 15:45:36 -0500

    Homeowkr

---
+++ 3401_Week2_Collins/Assets/GameScene/Scripts/AIMotor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class AIMotor : MonoBehaviour
+{
+
+    public NavMeshAgent agent;
+    public Transform destination;
+    public float movespeed = 100;
+   
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+
+        agent.SetDestination(destination.position);
+
+        movespeed -= Time.deltaTime;
+
+    }
+}
